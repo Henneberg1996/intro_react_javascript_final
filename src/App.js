fb1d29979532c956a7b4dc4b7300d0081f738227
jhs <hdc74fhe@gmail.com>
2023-11-13 23:38:17 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App () {
+  const [name, setName] = useState('')
+  const [age, setAge] = useState(null)
+  const [persons, setPersons] = useState([
+    {"name": "jesper", "age": 26},
+    {"name": "Sara", "age": 90},
+    {"name": "Torsten", "age": 22},
+  ])
+
+const updatePerson = (key) => {
+  const updatedPersons = [...persons]
+  updatedPersons[key]['name'] = name
+  updatedPersons[key]['age'] = age
+  setName('name')
+  setAge('age')
+  alert(persons[key]['name'] + persons[key]['age'])
+
 }
 
-export default App;
+  
+
+  return (
+    <div className="Container" >
+
+        <div className="List">
+          {persons && (
+            persons.map((person, idx) => {
+              return (
+                <div className="DictListElement">
+                <input onChange={(e) => setName(e.target.value) } placeholder={person['name']} className="DictItemCenter" />
+                <input onChange={(e) => setAge(e.target.value)} placeholder={person['age']} className="DictItemCenter" />
+                <button onClick={() => updatePerson(idx)}  > update person </button>
+              </div>
+              )
+            })
+          )}
+
+        </div>
+
+
+    </div>
+
+
+  )
+
+}
+
+export default App
