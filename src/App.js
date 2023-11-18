b160cfb4e8dd9214ec2ac976e66c4fa35ca5b349
jhs <hdc74fhe@gmail.com>
2023-11-18 01:54:40 +0100

    changes added

--- src/App.js
+++ src/App.js
@@ -11,43 +11,49 @@ function App () {
       {'title': 'Ingloriuos Bastards', "rating": 10, "genre": 'Action' },
   ])
 
-const updateMovies = (key) => {
+const updateMovies = (idx, key, value) => {
   const updatedMovies = [...movies]
-  updatedMovies[key]['title'] = title != '' ? title: updatedMovies[key]['title'] 
-  updatedMovies[key]['rating'] = rating != null ? rating: updatedMovies[key]['rating'] 
-  updatedMovies[key]['genre'] = genre != '' ? genre : updatedMovies[key]['genre'] 
+  updatedMovies[idx][key] = value
   setMovies(updatedMovies)
-  setTitle('')
-  setRating(null)
-  setGenre('')
-  alert(movies[key]['title'] + " " + movies[key]['genre'] + " " + movies[key]["rating"] )
+
 }
 
 
   return (
     <div className="Container">
-        <div className="List">
-          <div className="HeaderText" >  Movies & Ratings </div>
-          <div style={{ display: 'flex', height: '10%',  width: '100%', borderBottom: '1px solid black', justifyContent: 'space-around'}} >
-            <div className="DictItemCenterLong" style={{alignSelf: 'end', fontWeight: '600'}}>Title</div>
-            <div className="DictItemCenterShort" style={{alignSelf: 'end', fontWeight: '600'}}>Rating</div>
-            <div className="DictItemCenter" style={{alignSelf: 'end', fontWeight: '600'}}>Genre</div>
-        <div className="DictItemCenter"/>
+       <div className="InputBox"> 
+        <div className="HeaderText">  Add movies </div>
+        <div style={{display: 'flex', justifyContent: 'start', alignItems: 'baseline', width: '90%', flexDirection:'column'}} >
+        <input onChange={(e) => setTitle(e.target.value)}  placeholder="Insert Title here" style={{width:'60%', border:'none', borderBottom: '1px solid black', fontSize: '15px', margin: '5%' }}  />
+        <input onChange={(e) => setRating(e.target.value)} placeholder="Insert Genre here" style={{width:'60%', border:'none', borderBottom: '1px solid black', fontSize: '15px', margin: '5%' }}  />
+        <input onChange={(e) => setGenre(e.target.value)} placeholder="Insert Rating here" style={{width:'25%', border:'none', borderBottom: '1px solid black', fontSize: '15px', margin: '5%' }}  />
+        <button style={{width:'50%', height:'40px', border:'1px solid black', fontSize: '15px', margin: '5%', justifyItems: 'center', alignItems: 'center', alignSelf: 'center', backgroundColor: '#61a4ad', color: 'white', fontSize: '15px' }}>Add movie</button>
         </div>
-            {movies && (
-                movies.map((movie, idx) => {
-                  return (
-                    <div key={idx} style={{ display: 'flex', height: '10%',  width: '100%', borderBottom: '1px solid black', justifyContent: 'space-around'}} >
-                      <input onChange={(e) => setTitle(e.target.value)}  defaultValue={movie["title"]} className="DictItemCenterLong" />
-                      <input onChange={(e) => setRating(e.target.value)} defaultValue={movie["rating"]} className="DictItemCenterShort" />
-                      <input onChange={(e) => setGenre(e.target.value)} defaultValue={movie['genre']} className="DictItemCenter"/>
-                    <button onClick={() => updateMovies(idx)} className="DictItemCenter">Change</button>
-                  </div>
-                  )
-                })
-            )
-            }
           </div>
+
+         <div className="List">
+        <div className="HeaderText" >  Movies & Ratings </div>
+        <div style={{ display: 'flex', height: '10%',  width: '100%', borderBottom: '1px solid black', justifyContent: 'space-around'}} >
+          <div className="DictItemCenterLong" style={{alignSelf: 'end', fontWeight: '600'}}>Title</div>
+          <div className="DictItemCenterShort" style={{alignSelf: 'end', fontWeight: '600'}}>Rating</div>
+          <div className="DictItemCenter" style={{alignSelf: 'end', fontWeight: '600'}}>Genre</div>
+      <div className="DictItemCenter"/>
+      </div>
+          {movies && (
+              movies.map((movie, idx) => {
+                if(movies[idx] != null)
+                {return (
+                  <div key={idx} style={{ display: 'flex', height: '10%',  width: '100%', borderBottom: '1px solid black', justifyContent: 'space-around'}} >
+                    <input onChange={(e) => updateMovies(idx, 'title', e.target.value)} placeholder={movie["title"]} value={movie["title"]} className="DictItemCenterLong" />
+                    <input onChange={(e) => updateMovies(idx, "rating", e.target.value)} placeholder={movie["rating"]} value={movie["rating"]} className="DictItemCenterShort" />
+                    <input onChange={(e) => updateMovies(idx, "genre", e.target.value)} placeholder={movie['genre']} value={movie['genre']} className="DictItemCenter"/>
+                  <button onClick={() => alert("Title: " + movies[idx]['title'] + " " + "Rating: " + " " + movies[idx]['rating'] + " " + "Genre: " + " " + movies[idx]['genre'])} className="DictItemCenter">Change</button>
+                </div>
+                )}
+              })
+          )
+          }
+        </div>
         </div>
   )
 
