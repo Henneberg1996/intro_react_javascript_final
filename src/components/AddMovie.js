4056b88e15a3dff1d17ac7376822e7149fb12f24
jhs <hdc74fhe@gmail.com>
2023-12-14 00:32:18 +0100

    url removed

--- src/components/AddMovie.js
+++ src/components/AddMovie.js
@@ -1,39 +1,49 @@
+function AddMovie({ titleAdd, ratingAdd, genreAdd, movies, setTitleAdd, setGenreAdd, setRatingAdd, setMovies }) {
+  // Funktion til at tilføje en ny film til databasen
+  const addMovie = async () => {
+    // Opretter en ny film baseret på input-felterne
+    const newMovie = { "title": titleAdd, "rating": ratingAdd, "genre": genreAdd };
 
-function AddMovie ({titleAdd, ratingAdd, genreAdd, movies, setTitleAdd, setGenreAdd, setRatingAdd, setMovies}) {
+    // Opretter en opdateret liste af film baseret på den eksisterende liste og den nye film
+    const updatedMovies = movies !== null ? [...movies, newMovie] : [newMovie];
 
-    const addMovie = async () => {
-        const newMovie = {"title": titleAdd, "rating": ratingAdd, "genre": genreAdd}
-        const updatedMovies = movies != null ? [...movies, newMovie] : [newMovie]
-        setTitleAdd("")
-        setGenreAdd("")
-        setRatingAdd("")
-  
-        const response = await fetch(`https://testdb-7e328-default-rtdb.europe-west1.firebasedatabase.app/movies/.json`, 
-        {
-          method: 'PUT',
-          body: JSON.stringify(updatedMovies)
-        }
-        )
+    // Nulstiller input-felterne
+    setTitleAdd("");
+    setGenreAdd("");
+    setRatingAdd("");
 
-        if (response.ok)
-        {
-          alert("success")
-          setMovies(updatedMovies)
-      }
+    // Sender en PUT-anmodning til databasen med den opdaterede liste af film
+    //Mit anvendte URL: https://testdb-7e328-default-rtdb.europe-west1.firebasedatabase.app/movies/.json
+    //INDSÆT DIT EGET URL HER
+    const response = await fetch(``, {
+      method: 'PUT',
+      body: JSON.stringify(updatedMovies)
+    });
 
-        else alert("fail")
-      }
-    return (
-        <div className="InputBox"> 
-        <div className="HeaderText">  Add movies </div>
-        <div style={{display: 'flex', justifyContent: 'start', alignItems: 'baseline', width: '90%', flexDirection:'column'}} >
-        <input onChange={(e) => setTitleAdd(e.target.value)} value={titleAdd} placeholder="Insert Title here" style={{width:'60%', border:'none', borderBottom: '1px solid black', fontSize: '15px', margin: '5%' }}  />
-        <input onChange={(e) => setGenreAdd(e.target.value)} value={genreAdd} placeholder="Insert Genre here" style={{width:'60%', border:'none', borderBottom: '1px solid black', fontSize: '15px', margin: '5%' }} />
-        <input onChange={(e) => setRatingAdd(e.target.value)} value={ratingAdd} placeholder="Insert Rating here" style={{width:'25%', border:'none', borderBottom: '1px solid black', fontSize: '15px', margin: '5%' }} />
-        <button onClick={addMovie} style={{width:'50%', height:'40px', border:'1px solid black', fontSize: '15px', margin: '5%', justifyItems: 'center', alignItems: 'center', alignSelf: 'center', backgroundColor: '#61a4ad', color: 'white', fontSize: '15px' }}>Add movie</button>
-        </div>
-          </div>
-    )
+    // Viser en besked afhængigt af anmodningens succes og opdaterer filmene, hvis det er vellykket
+    if (response.ok) {
+      alert("success");
+      setMovies(updatedMovies);
+    } else {
+      alert("fail");
+    }
+  };
 
+  // Returnerer AddMovie-komponenten
+  return (
+    <div className="InputBox">
+      {/* Overskrift for tilføjelse af film */}
+      <div className="HeaderText">  Add movies </div>
+      {/* Input-felter til titel, genre og rating med tilhørende knap til at tilføje film */}
+      <div style={{ display: 'flex', justifyContent: 'start', alignItems: 'baseline', width: '90%', flexDirection: 'column' }}>
+        <input onChange={(e) => setTitleAdd(e.target.value)} value={titleAdd} placeholder="Insert Title here" style={{ width: '60%', border: 'none', borderBottom: '1px solid black', fontSize: '15px', margin: '5%' }} />
+        <input onChange={(e) => setGenreAdd(e.target.value)} value={genreAdd} placeholder="Insert Genre here" style={{ width: '60%', border: 'none', borderBottom: '1px solid black', fontSize: '15px', margin: '5%' }} />
+        <input onChange={(e) => setRatingAdd(e.target.value)} value={ratingAdd} placeholder="Insert Rating here" style={{ width: '25%', border: 'none', borderBottom: '1px solid black', fontSize: '15px', margin: '5%' }} />
+        <button onClick={addMovie} style={{ width: '50%', height: '40px', border: '1px solid black', fontSize: '15px', margin: '5%', justifyItems: 'center', alignItems: 'center', alignSelf: 'center', backgroundColor: '#61a4ad', color: 'white', fontSize: '15px' }}>Add movie</button>
+      </div>
+    </div>
+  );
 }
-export default AddMovie
+
+// Eksporterer AddMovie-komponenten som standard
+export default AddMovie;
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,64 +1,90 @@
-
-function MovieList ({title, rating, genre, movies, setTitle, setGenre, setRating}) {
+function MovieList ({ title, rating, genre, movies, setTitle, setGenre, setRating }) {
+  // Logger filmlisten til konsollen
   console.log("movies")
   console.log(movies)
+
+  // Funktion til at opdatere en film i databasen
   const updateMovies = async (idx) => {
-      const newMovie = {
-        "title": title != "" ? title : movies[idx]['title'], 
-        "rating": rating != null ? rating : movies[idx]['rating'], 
-        "genre": genre != "" ? genre : movies[idx]['genre']
-      }
-      const response = await fetch(`https://testdb-7e328-default-rtdb.europe-west1.firebasedatabase.app/movies/${idx}.json`, 
+    // Opretter en ny film med enten de nye input-værdier eller de eksisterende værdier fra den valgte film
+    const newMovie = {
+      "title": title !== "" ? title : movies[idx]['title'],
+      "rating": rating !== null ? rating : movies[idx]['rating'],
+      "genre": genre !== "" ? genre : movies[idx]['genre']
+    }
+
+    // Sender en PATCH-anmodning til databasen med den nye film
+    //Mit anvendte URL: https://testdb-7e328-default-rtdb.europe-west1.firebasedatabase.app/movies/${idx}.json
+    //INDSÆT DIT EGET URL HER
+    const response = await fetch(``,
       {
         method: 'PATCH',
         body: JSON.stringify(newMovie)
       }
-      )
-      setTitle("")
-      setRating(null)
-      setTitle("")
-      if (response.ok){alert("success")} else alert("fail")
+    )
+
+    // Nulstiller input-felterne og viser en besked afhængigt af anmodningens succes
+    setTitle("")
+    setRating(null)
+    setTitle("")
+    if (response.ok) {
+      alert("success")
+    } else {
+      alert("fail")
     }
+  }
 
-    const deleteMovie = async (idx) => {
-
-const response = await fetch(`https://testdb-7e328-default-rtdb.europe-west1.firebasedatabase.app/movies/${idx}.json`, 
+  // Funktion til at slette en film fra databasen
+  const deleteMovie = async (idx) => {
+    //Sender en DELETE-anmodning til databasen for at slette filmen
+    //Mit anvendte URL: https://testdb-7e328-default-rtdb.europe-west1.firebasedatabase.app/movies/${idx}.json
+    //INDSÆT DIT EGET URL HER
+    const response = await fetch(``,
       {
         method: 'DELETE',
       }
-      )
-      if (response.ok){alert("success")} else alert("fail")
-
-
-    }
-    
-
-    return(
-        <div className="List">
-        <div className="HeaderText" >  Movies & Ratings </div>
-        <div style={{ display: 'flex', height: '10%',  width: '100%', borderBottom: '1px solid black', justifyContent: 'space-around'}} >
-          <div className="DictItemCenterLong" style={{alignSelf: 'end', fontWeight: '600'}}>Title</div>
-          <div className="DictItemCenterShort" style={{alignSelf: 'end', fontWeight: '600'}}>Rating</div>
-          <div className="DictItemCenter" style={{alignSelf: 'end', fontWeight: '600'}}>Genre</div>
-      <div style={{width: '51%'}} className="DictItemCenter"/>
-      </div>
-          {movies && (
-              movies.map((movie, idx) => {
-                if(movies[idx] != null)
-                {return (
-                  <div key={idx} style={{ display: 'flex', height: '10%',  width: '100%', borderBottom: '1px solid black', justifyContent: 'space-around'}} >
-                    <input onChange={(e) => setTitle(e.target.value)} placeholder={movie["title"]} defaultValue={movie["title"]} className="DictItemCenterLong" />
-                    <input onChange={(e) => setRating(e.target.value)} placeholder={movie["rating"]} defaultValue={movie["rating"]} className="DictItemCenterShort" />
-                    <input onChange={(e) => setGenre(e.target.value)} placeholder={movie['genre']} defaultValue={movie['genre']} className="DictItemCenter"/>
-                  <button onClick={() => updateMovies(idx) } className="DictItemCenter">Change</button>
-                  <button onClick={() => deleteMovie(idx) } className="DictItemCenter">Delete</button>
-                </div>
-                )}
-              })
-          )
-          }
-        </div>
     )
 
+    // Viser en besked afhængigt af anmodningens succes
+    if (response.ok) {
+      alert("success")
+    } else {
+      alert("fail")
+    }
+  }
+
+  // Returnerer MovieList-komponenten
+  return (
+    <div className="List">
+      {/* Overskrift for filmlisten */}
+      <div className="HeaderText" > Movies & Ratings </div>
+      {/* Header-row med kolonnenavne */}
+      <div style={{ display: 'flex', height: '10%', width: '100%', borderBottom: '1px solid black', justifyContent: 'space-around' }} >
+        <div className="DictItemCenterLong" style={{ alignSelf: 'end', fontWeight: '600' }}>Title</div>
+        <div className="DictItemCenterShort" style={{ alignSelf: 'end', fontWeight: '600' }}>Rating</div>
+        <div className="DictItemCenter" style={{ alignSelf: 'end', fontWeight: '600' }}>Genre</div>
+        <div style={{ width: '51%' }} className="DictItemCenter" />
+      </div>
+      {/* Mapper igennem filmene og viser dem i rækker med redigerings- og sletknapper */}
+      {movies && (
+        movies.map((movie, idx) => {
+          if (movies[idx] != null) {
+            return (
+              <div key={idx} style={{ display: 'flex', height: '10%', width: '100%', borderBottom: '1px solid black', justifyContent: 'space-around' }} >
+                {/* Input-felter med placeholder og defaultværdier fra filmen */}
+                <input onChange={(e) => setTitle(e.target.value)} placeholder={movie["title"]} defaultValue={movie["title"]} className="DictItemCenterLong" />
+                <input onChange={(e) => setRating(e.target.value)} placeholder={movie["rating"]} defaultValue={movie["rating"]} className="DictItemCenterShort" />
+                <input onChange={(e) => setGenre(e.target.value)} placeholder={movie['genre']} defaultValue={movie['genre']} className="DictItemCenter" />
+                {/* Knapper til at opdatere eller slette filmen */}
+                <button onClick={() => updateMovies(idx)} className="DictItemCenter">Change</button>
+                <button onClick={() => deleteMovie(idx)} className="DictItemCenter">Delete</button>
+              </div>
+            )
+          }
+        })
+      )}
+    </div>
+  )
 }
+
+// Eksporterer MovieList-komponenten som standard
 export default MovieList
